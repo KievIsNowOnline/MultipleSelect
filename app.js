447b9c98ee66364994f8b12751d6359b4b54d658
chicken <kevinisnowonline@gmail.com>
2014-05-21 17:45:17 +0800

    Included initial binding for list-item directive. Now event is being triggered! :)

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 var app = angular.module('myModule', []);
 
+app.controller('myController', function($scope){
+	$scope.items = [
+		{
+			id:1,
+			value:'Option 1'
+		},
+		{
+			id:2,
+			value:'Option 2'
+		},
+		{
+			id:3,
+			value:'Option 3'
+		},
+		{
+			id:4,
+			value:'Option 4'
+		}
 
-console.log(app);
+	];
+
+	console.log($scope.items);
+});
+
--- multiSelect.js
+++ multiSelect.js
@@ -1,12 +1,33 @@
 
 app.directive('multiSelect',function(){
 	return {
-		restrict: 'ECMA',
-		
-		
+		restrict: 'C',
 		templateUrl :'/multiSelectTemplate.html',
-		replace:true
+		replace:true,
+		scope:{
+			items: '='
+		},
+		controller: ['$scope', function($scope){
+
+		}],
 	}
 
 
+});
+
+app.directive('mayItem', function(){
+	return{
+		require: '^multiSelect',
+		restrict: 'C',
+		   link: function (scope, element, attrs, controller) {
+              
+
+                //function that binds the <li> element mousedown() event
+                //click is not used as it brings race condition on onBlur and click of the <input> element
+                element.bind('mousedown', function () {
+                	alert('you have clicked a line item');
+                });
+            }
+
+	}
 });
